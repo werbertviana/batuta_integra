7ad8ddf46e4a1286e64ef0ae689b26a766e4b95c
Werbert Viana <werbertbrito@gmail.com>
2021-09-30 11:09:02 -0300

    atualização screens

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,3 +2,9 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from '../screens/Home/Home';
+
+const Tab = createBottomTabNavigator();
+
+export default function Routes(){
+    
+}
