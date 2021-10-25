ec167aced48d92ff94ba02439dda4dfef30daaa8
Werbert Viana <werbertbrito@gmail.com>
2021-10-25 07:47:06 -0300

    sliders

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import Navegacao from './src/navegacao';
+import Navegacao from './src/screens/Conteudo/Licao01/Intro';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => Navegacao);
--- src/screens/Conteudo/Licao01/Intro.js
+++ src/screens/Conteudo/Licao01/Intro.js
@@ -1,10 +1,110 @@
+//import bibliotecas
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import FastImage from 'react-native-fast-image';
 
-export default function App(){
+//import images
+import Introducao from '../../../assets/imgs/introducao.png';
+import slides01 from '../../../assets/imgs/Conteudo/Licao01/slides01.png';
+import slides02 from '../../../assets/imgs/Conteudo/Licao01/slides02.png';
+import slides03 from '../../../assets/imgs/Conteudo/Licao01/slides03.png';
+import slides04 from '../../../assets/imgs/Conteudo/Licao01/slides04.png';
+import slides05 from '../../../assets/imgs/Conteudo/Licao01/slides05.png';
+
+//import slides estaticos
+import staticSlides from '../../../data/Conteudo/Licao01/Intro.json'
+
+//import estilos
+import {
+    DivisorLine2,
+    Bgcontainer
+} from '../../../components/style'
+
+export default function App() {
+
+    const allSlides = staticSlides.slides;
+
+    const renderHeader = () => {
+        return (
+            <SafeAreaView style={styles.HeaderContainer}>
+                <FastImage source={Introducao} style={{ width: 200, height: 70 }}></FastImage>
+            </SafeAreaView>
+        )
+    }
+
+    function renderSlides({ item }) {
+        switch (item.image) {
+            case ("slides01.png"):
+                return (
+                    <SafeAreaView>
+                        <FastImage source={slides01} style={{ width: 50, height: 50 }}>
+                        </FastImage>
+                    </SafeAreaView>
+                )
+                break;
+            case ("slides02.png"):
+                return (
+                    <SafeAreaView>
+                        <FastImage source={slides02} style={{ width: 50, height: 50 }}>
+                        </FastImage>
+                    </SafeAreaView>
+                )
+                break;
+            case ("slides03.png"):
+                return (
+                    <SafeAreaView>
+                        <FastImage source={slides03} style={{ width: 50, height: 50 }}>
+                        </FastImage>
+                    </SafeAreaView>
+                )
+                break;
+            case ("slides04.png"):
+                return (
+                    <SafeAreaView>
+                        <FastImage source={slides04} style={{ width: 50, height: 50 }}>
+                        </FastImage>
+                    </SafeAreaView>
+                )
+                break;
+            case ("slides05.png"):
+                return (
+                    <SafeAreaView>
+                        <FastImage source={slides05} style={{ width: 50, height: 50 }}>
+                        </FastImage>
+                    </SafeAreaView>
+                )
+                break;
+        }
+    }
+    
     return (
-        <SafeAreaView>
+        <Bgcontainer>
+            {/* Header*/}
             
-        </SafeAreaView>
+            <Text>Teste</Text>
+            {/* Slides */}
+            {/* <DivisorLine2></DivisorLine2>
+            <AppIntroSlider
+                renderItem={renderSlides}
+                data={allSlides}
+                activeDotStyle={{
+                    backgroundColor: '#96989A'
+                }}
+            >
+            </AppIntroSlider> */}
+        </Bgcontainer>
     )
+
 }
+
+const styles = StyleSheet.create({
+    HeaderContainer: {
+        marginTop: '1%',
+        width: '100%',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: 'red'
+    }
+})
