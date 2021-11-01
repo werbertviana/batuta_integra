3446f79c4fed7078e7538ee9f9c35219b3fad74f
Elleven <werbertbrito@gmail.com>
2021-11-01 17:42:58 -0300

    sound

--- src/screens/Conteudo/Licao01/Intro.js
+++ src/screens/Conteudo/Licao01/Intro.js
@@ -35,7 +35,24 @@ import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 export default function App({ navigation }) {
 
     var Sound = require('react-native-sound');
-
+    
+    var whoosh = new Sound('whoosh.mp3', Sound.MAIN_BUNDLE, (error) => {
+        if (error) {
+          console.log('failed to load the sound', error);
+          return;
+        }
+        // loaded successfully
+        console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+      
+        // Play the sound with an onEnd callback
+        whoosh.play((success) => {
+          if (success) {
+            console.log('successfully finished playing');
+          } else {
+            console.log('playback failed due to audio decoding errors');
+          }
+        });
+      });
    
 
     const allSlides = staticSlides.slides;
